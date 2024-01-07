686631f1f04ca10a8055d7710de089eb0cf732a4
HaoxuanYv <yhxuan77@163.com>
2024-01-07 13:36:19 +0800

    final

---
+++ next.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    swcPlugins: [["next-superjson-plugin", {}]]
+  },
+  images: {
+
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '**',
+      },
+  ]
+  }
+}
+
+module.exports = nextConfig
